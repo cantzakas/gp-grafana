343a3311b428d4530e6b4ea260e56e3f95189279
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2019-11-07 11:55:51 +0000

    Update install-telegraf-1.12.4-1.sh

--- install-telegraf-1.12.4-1.sh
+++ install-telegraf-1.12.4-1.sh
@@ -2,6 +2,6 @@
 
 sudo yum localinstall telegraf-1.12.4-1.x86_64.rpm
 
-systemctl daemon-reload
+#systemctl daemon-reload
 #systemctl enable influxdb.service
-systemctl start telegraf
+#systemctl start telegraf
