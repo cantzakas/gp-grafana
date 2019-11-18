dd03f563e50a5ba859f4a0b257776dcdb1a86c9a
Costas <cantzakas@pivotal.io>
2019-11-18 15:39:29 +0000

    update start-telegraf.sh

--- start-telegraf.sh
+++ start-telegraf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-orgalorg -o mdw -o sdw1 -o sdw2 -o sdw3 -o sdw4 -o sdw5 -o sdw6 -o sdw7 -o sdw8 -C sudo systemctl start telegraf
+source ~/.bash_profile
+
+orgalorg -o ./hostfile_allhosts -v -C sudo systemctl start telegraf
